0e3fde0bffee88aed1df5d5be9e83c65c827ba0d
vishnu <vishnudileep8520@gmail.com>
2026-04-05 23:31:40 +0530

    inital commit

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ const isLocalHost =
 const LOCAL_API_URL = "http://localhost:5000/api";
 
 // Update this before deploying the frontend.
-const PRODUCTION_API_URL = "https://your-render-backend.onrender.com/api";
+const PRODUCTION_API_URL = "https://expense-backend-odv1.onrender.com/";
 
 export const API_URL = isLocalHost ? LOCAL_API_URL : PRODUCTION_API_URL;
